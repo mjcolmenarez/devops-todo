a605fbe4f0ab07634425ad5747470b4cb31ed7f2
Maria Jose Colmenarez <mariajosecolmenarez4@gmail.com>
2025-09-30 22:01:43 +0200

    add CRUD and URL wiring

---
+++ tasks/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import TaskList, TaskCreate, TaskUpdate, TaskDelete, toggle_done, signup
+
+app_name = "tasks"
+urlpatterns = [
+    path("signup/", signup, name="signup"),
+    path("", TaskList.as_view(), name="list"),
+    path("new/", TaskCreate.as_view(), name="create"),
+    path("<int:pk>/edit/", TaskUpdate.as_view(), name="update"),
+    path("<int:pk>/delete/", TaskDelete.as_view(), name="delete"),
+    path("<int:pk>/toggle/", toggle_done, name="toggle"),
+]
---
+++ tasks/views.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Task
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/signup.html", {"form": form})
+
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "tasks/list.html"
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ["title", "description", "due_date"]
+    template_name = "tasks/form.html"
+    success_url = reverse_lazy("tasks:list")
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ["title", "description", "due_date", "is_done"]
+    template_name = "tasks/form.html"
+    success_url = reverse_lazy("tasks:list")
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = "tasks/confirm_delete.html"
+    success_url = reverse_lazy("tasks:list")
+
+@login_required
+def toggle_done(request, pk):
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    task.is_done = not task.is_done
+    task.save()
+    return redirect("tasks:list")
